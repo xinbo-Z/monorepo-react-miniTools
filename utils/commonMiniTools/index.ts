7e6be90bf9b636a1724844a17205b796ef9b9e3c
zhangxinbo@vastchain.ltd <zhangxinbo@vastchain.ltd>
2024-10-17 00:23:43 +0800

    feat: 初始化仓库

---
+++ utils/commonMiniTools/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author       : ZTC-张添财
+ * @Version      : V1.0
+ * @Date         : 2024-10-15 23:37:50
+ * @Description  : 
+ */
+export default function () {
+    return 222
+}
---
+++ utils/modalManage/index.ts
@@ -0,0 +1,6 @@
+/*
+ * @Author       : ZTC-张添财
+ * @Version      : V1.0
+ * @Date         : 2024-10-15 23:38:23
+ * @Description  : 
+ */
